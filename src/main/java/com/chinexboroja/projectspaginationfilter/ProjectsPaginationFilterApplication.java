bce8617467a7db0b37c9076320ba012ca27d3bc9
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2024-04-09 22:37:25 +0100

    init setups

---
+++ src/main/java/com/chinexboroja/projectspaginationfilter/ProjectsPaginationFilterApplication.java
@@ -0,0 +1,13 @@
+package com.chinexboroja.projectspaginationfilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectsPaginationFilterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectsPaginationFilterApplication.class, args);
+    }
+
+}
